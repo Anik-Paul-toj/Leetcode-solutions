bba6c5555dd9e5616debd6a3feb612951e16b326
Anik Paul <trojanik003@gmail.com>
2026-04-30 00:08:26 +0530

    d

--- Strings/largest_number.py
+++ Strings/largest_number.py
@@ -14,3 +14,28 @@
 # Example 2:
 # Input: nums = [3,30,34,5,9]
 # Output: "9534330"
+
+## Approach 
+# 1. Convert all integers to strings for easy comparison.
+# 2. Sort the list of strings based on a custom comparison function that compares the 
+#       concatenation of two strings in both possible orders (e.g., "30" + "3" vs "3" + "30").
+# 3. Join the sorted list of strings to form the largest number.
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        for i,n in enumerate(nums):
+            nums[i] = str(n)
+        print(nums)
+
+        def com(n1,n2):
+            return n1+n2 > n2+n1
+
+        n = len(nums)
+        for i in range(n):
+            for j in range(n - 1):
+                if not com(nums[j], nums[j + 1]):
+                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
+        result = ''.join(nums)
+        if result[0] == '0':
+            return '0'
+        else:
+            return result
